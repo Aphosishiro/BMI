30362b345ce6d92fe3c1d7f989b72753158f784c
Olumi Yimika <Aphosishiro@gmail.com>
2023-06-23 20:51:04 +0100

    layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/screens/reusablecard.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,13 +12,40 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
       appBar: AppBar(
         leading: const Icon(Icons.menu),
         title: const Text("BMI CALCULATOR"),
         centerTitle: true,
       ),
-      body: Column(),
+      body: const Column(
+        children: [
+          Row(
+            children: [
+              ReuseableCard(
+                color: Colors.black,
+              ),
+              ReuseableCard(
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              ReuseableCard(color: Colors.black),
+            ],
+          ),
+          Row(
+            children: [
+              ReuseableCard(
+                color: Colors.black,
+              ),
+              ReuseableCard(
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/reusablecard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ReuseableCard extends StatelessWidget {
+  const ReuseableCard({super.key, required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: size.width * 0.05,
+          vertical: size.height * 0.01,
+        ),
+        height: size.height * 0.24,
+        width: size.width * 0.35,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
